6847c65e04d3ef21c4a885dce558976f44b03f31
abubakkarsiddik123 <abu089604@gmail.com>
2026-07-09 03:27:30 +0600

    implement delete and update oparation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
-const uri =process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,37 +11,70 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', async(req, res)=>{
-    res.send('Server is runnign fine!')
-})
-
+app.get("/", async (req, res) => {
+  res.send("Server is runnign fine!");
+});
 
 async function run() {
   try {
     await client.connect();
-    const db=client.db("wanderlust");
-    const destinationCollection=db.collection("destinations");
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destinations");
 
-    app.post("/destinations", async(req, res)=>{
-      const destinationData = req.body;
-      const result= await destinationCollection.insertOne(destinationData)
+    app.get("/destination", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
       res.send(result);
+    });
 
+    app.post("/destinations", async (req, res) => {
+      const destinationData = req.body;
+      const result = await destinationCollection.insertOne(destinationData);
+      res.send(result);
+    });
+
+    app.get("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await destinationCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updateData = req.body;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await destinationCollection.updateOne(
+         query ,
+        { $set: updateData },
+      );
+      res.send(result)
+    });
+
+    app.delete("/destination/:id", async(req, res)=>{
+      const {id}=req.params;
+      const query = {
+        _id:new ObjectId(id)
+      }
+      const result = await destinationCollection.deleteOne(query);
+      res.send(result)
     })
 
-    
-
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
